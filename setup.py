169c6135c34ba5198c4ba2da5d7f2916f5991183
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-05-09 00:12:27 +0200

    Create CI based update flow
    Moves the update of the IANA top-level data into a CI based workflow, to
    allow building this package in a sandboxed environment, where network
    access is not allowed.
    
    Also creates tags for each new data version.

--- setup.py
+++ setup.py
@@ -1,56 +1,26 @@
-import os
-import re
-import sys
-from distutils.command.build_py import build_py
+from setuptools import setup
 
-from setuptools import setup, find_packages
-
-if sys.version_info.major >= 3:
-    import urllib.request
-
-    r = urllib.request.urlopen('https://data.iana.org/TLD/tlds-alpha-by-domain.txt')
-    assert r.status == 200
-    data = r.read().decode('utf-8').split('\n')
-else:
-    import urllib
-    
-    r = urllib.urlopen('https://data.iana.org/TLD/tlds-alpha-by-domain.txt')
-    assert r.getcode() == 200
-    data = r.read().split('\n')
-    
-version = re.match('^# Version (?P<version>[0-9]+).*$', data[0]).group('version')
-tlds = [i.lower() for i in data[1:] if i and not i.startswith('#')]
+with open("./version") as fd:
+    version = fd.read().strip()
 
 
-class build_tld_py(build_py):
-    def run(self):
-        if not self.dry_run:
-            target_dir = os.path.join(self.build_lib, 'tlds')
-            self.mkpath(target_dir)
-
-            with open(os.path.join(target_dir, '_data.py'), 'w') as f:
-                f.write('tld_set = set(%s)\n' % (tlds, ))
-
-        build_py.run(self)
-
-
-setup(name='tlds',
-      version=version,
-      description='Automatically updated list of valid TLDs taken directly from IANA',
-      long_description=open('README.rst').read(),
-      classifiers=[
-          'Development Status :: 5 - Production/Stable',
-          'Programming Language :: Python',
-          'License :: OSI Approved :: MIT License',
-          'Topic :: Communications',
-      ],
-      keywords='tld',
-      author='Amir Szekely',
-      author_email='kichik@gmail.com',
-      url='https://github.com/kichik/tlds',
-      license='MIT',
-      packages=['tlds'],
-      cmdclass={'build_py': build_tld_py},
-      include_package_data=True,
-      zip_safe=True,
-      )
+setup(
+    name="tlds",
+    version=version,
+    description="Automatically updated list of valid TLDs taken directly from IANA",
+    long_description=open("README.rst").read(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Communications",
+    ],
+    keywords="tld",
+    author="Amir Szekely",
+    author_email="kichik@gmail.com",
+    url="https://github.com/kichik/tlds",
+    license="MIT",
+    packages=["tlds"],
+    include_package_data=True,
+    zip_safe=True,
+)
